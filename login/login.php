205cff4cd917475869fefedf1234fbb67e452784
GabrielFachinello09 <gabrielfachinello134@gmail.com>
2026-09-22 08:48:00 -0300

    adicionando verificação para login

---
+++ login/login.php
@@ -0,0 +1,47 @@
+<?php require_once '../includes/functions.php'; 
+session_start();?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+    <body style="text-align:center;">
+
+        <h1>Faça Login</h1>
+    <?php include '../includes/header.php'; ?>
+        <br>
+        <form action="" method="post">
+            <label for="email">Digite seu E-mail: </label>
+            <input type="text" name="email" id="email" required><br>
+
+            <label for="senha">Digite sua Senha: </label>
+            <input type="password" name="senha" id="senha" required><br>
+
+            <input type="submit" value="Fazer Login">
+        </form>
+
+       
+   
+
+    <?php
+        if($_SERVER['REQUEST_METHOD']=="POST") {
+            $usuario = consulta_user($conexao,$_POST['email']);
+            if($_POST['email'] == $usuario['email'] && $_POST['senha'] == $usuario['senha']){
+                $_SESSION['id'] = $usuario['id'];
+                echo "Login realizado com sucesso, você será redirecionado!";
+                sleep(3);
+                header("Location: ../index.php");
+                exit();
+                
+            } else {
+                echo "Usuário ou senha inválidos.";
+            }
+        }
+    ?>
+
+    <?php include '../includes/footer.php'; ?>
+</body>
+</html>
---
+++ login/verifica_user.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../includes/functions.php';
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    header("Location: ../login/login.php");
+    exit();
+}
+
+?>
